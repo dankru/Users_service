0a44e97adb78c9e8eaced2e7445a7f3c846e568c
dankru <noiafugace@gmail.com>
2025-02-13 02:50:35 +0700

    move password for aesthetic reason

--- cmd/main.go
+++ cmd/main.go
@@ -17,8 +17,8 @@ func main() {
 		DB_HOST:     os.Getenv("DB_HOST"),
 		DB_PORT:     os.Getenv("DB_PORT"),
 		DB_USER:     os.Getenv("DB_USER"),
-		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
 		DB_NAME:     os.Getenv("DB_NAME"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
 	}
 	postgres := pgsql.NewPostgreSQLDB(conn)
 	defer postgres.Close()
